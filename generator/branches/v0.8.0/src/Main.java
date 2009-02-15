8658155d0b93d3347b2bc66852f6c98f9a8e4845
achim.weimert <achim.weimert@d8d4e5e2-ead3-11dd-b882-2156224b14b4>
2009-02-15 13:09:12 +0000

    release version 0.8.0

---
+++ generator/branches/v0.8.0/src/Main.java
@@ -0,0 +1,85 @@
+/*******************************************************************************
+ * This file is part of zdt2go generator.
+ * Copyright (c) 2009 Achim Weimert.
+ * http://code.google.com/p/zdt2go/
+ * 
+ * zdt2go generator is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * zdt2go generator is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with zdt2go generator.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Contributors:
+ *     Achim Weimert - initial API and implementation
+ ******************************************************************************/
+import java.io.File;
+
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+
+
+
+public class Main {
+	
+	public static void main(String[] args) {
+		int result;
+		Generator generator = new Generator();
+		String outputDirectory;
+		
+		if (args.length==0) {
+			JFileChooser directoryChoose = new JFileChooser();
+			directoryChoose.setDialogTitle("Select output directory");
+			directoryChoose.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+			result = directoryChoose.showOpenDialog(null);
+			if (result == JFileChooser.CANCEL_OPTION) {
+				return;
+			}
+	
+			outputDirectory = directoryChoose.getSelectedFile().getAbsolutePath();
+			generator.setOutputDirectory(outputDirectory);
+	
+			if (generator.existsOutputFile()) {
+		    	result = JOptionPane.showConfirmDialog(null, "Output file exists.\n\nOverwrite?", "Warning", JOptionPane.YES_NO_OPTION);
+		    	if (result==JOptionPane.NO_OPTION) {
+		    		return;
+		    	}
+			}
+			
+			JFileChooser fileChoose = new JFileChooser();
+			fileChoose.setDialogTitle("Select vocabulary files or directories. Press Ctrl to select multiple files.");
+			fileChoose.setMultiSelectionEnabled(true);
+			fileChoose.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+			result = fileChoose.showOpenDialog(null);
+			if (result == JFileChooser.CANCEL_OPTION) {
+				return;
+			}
+	
+			File[] files = fileChoose.getSelectedFiles();
+	
+			for (int i=0; i<files.length; i++) {
+				generator.recursivelyAddFiles(files[i].getAbsolutePath());
+			}
+		} else {
+			outputDirectory = System.getProperty("user.dir");
+			for (int i=0; i<args.length; i++) {
+				generator.recursivelyAddFiles(args[i]);
+			}
+		}
+
+		generator.createJarAndJad();
+		JOptionPane
+				.showMessageDialog(
+						null,
+						"Done.\n\nCheck the output directory\n"
+								+ outputDirectory + "\n"
+								+ "for the files "+Generator.OUTPUT_JAR_FILE+" and "+Generator.OUTPUT_JAD_FILE+"\n\n"
+								+ "To start studying transfer them to your mobile phone!");
+	}
+}
